acf401518ac550dfac0f2fc235e7da9846906f57
Ruksna khatun <iitianruksana@gmail.com>
2026-04-16 21:18:22 +0530

    added submission featude and admin

--- server.js
+++ server.js
@@ -16,17 +16,17 @@ const authRoutes = require('./routes/authRoutes');
 const playerRoutes = require('./routes/playerRoutes');
 const testRoutes = require('./routes/testRoutes');
 const documentRoutes = require('./routes/documentRoutes');
-// const submissionRoutes = require('./routes/submissionRoutes');
-// const aiRoutes = require('./routes/aiRoutes');
-// const adminRoutes = require('./routes/adminRoutes');
+const submissionRoutes = require('./routes/submissionRoutes');
+const aiRoutes = require('./routes/aiRoutes');
+const adminRoutes = require('./routes/adminRoutes');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/player', playerRoutes);
 app.use('/api/tests', testRoutes);
 app.use('/api/documents', documentRoutes);
-// app.use('/api/submissions', submissionRoutes);
-// app.use('/api/ai', aiRoutes);
-// app.use('/api/admin', adminRoutes);
+app.use('/api/submissions', submissionRoutes);
+app.use('/api/ai', aiRoutes);
+app.use('/api/admin', adminRoutes);
 
 // Test Route
 app.get('/', (req, res) => {
